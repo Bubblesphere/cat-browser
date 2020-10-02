3a64112c754e37f2dd7c505f01d7395e2da99e53
Bubblesphere <dallaire.deric@gmail.com>
2020-10-01 21:08:01 -0400

    Added CatGallery component

--- pages/cats.tsx
+++ pages/cats.tsx
@@ -1,19 +1,29 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import useCatService from '../hooks/useCatService';
+import styles from '../styles/Cats.module.css'
+import { CatGallery } from './shared/catGallery';
 
 export default function Cats() {
   const catService = useCatService();
   const [page, setPage] = useState(1);
   
   const { data, error } = catService.allBreeds(1, page);
-
+  
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
-
-  return <ul>
-    {data.map(x =>
-      <li>{x.name} - {x.life_span}</li>
-    )}
+  
+  const breed = data[0];
+  return <div>
+    <div>
+      <h1 className={styles.breed}>{breed.name}</h1>
+      <p>{breed.temperament}</p>
+      <p>{breed.life_span}</p>
+      <p>{breed.origin}</p>
+    </div>
+    <div>
+      <CatGallery breedId={breed.id}/>
+    </div>
     <button onClick={() => setPage(page + 1)}>Next</button>
-  </ul>
+  </div>
 }
+
---
+++ pages/shared/catGallery.tsx
@@ -0,0 +1,12 @@
+import useCatService from "../../hooks/useCatService";
+import styles from '../../styles/CatGallery.module.css'
+
+export function CatGallery(props) {
+    const catService = useCatService();
+    const { data, error } = catService.getBreedImages(props.breedId, 4, 1);
+  
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+  
+    return <ul>{data.map(x => <li><img src={x.url} className={styles.image}/></li>)}</ul>
+  }
